443174da8c82a6ea95f4ce8c3ae48c706b349572
x1qu <x1qu@ucsd.edu>
2022-11-15 09:33:58 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -5,3 +5,21 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+javac TestListExamples.java
+
+if [ $? != 00 ]
+then
+  echo "Missing Files! You need to do better than this! There is just one file
+  to be submitted and u messed up! Major disappointment!"
+  exit 1
+fi
+
+echo "U got all the files, good work, young man! But you would probably fail as well
+so drop this class right now!"
+
+if <-e ListExamples.java>
+then
+
+cp list-examples-grader
+
+mv 
